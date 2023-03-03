e1d9ff714cb9280eb367db18b9672b3cc4f087dc
RaphielSh <raphielsh@gmail.com>
2023-03-03 03:40:09 -0500

    It can convert numbers in range 0 to 4095 now.

--- bin.asm
+++ bin.asm
@@ -1,49 +1,28 @@
 section .data
-req db "Type a number:", 0xA
-req_len equ $-req
 cnst db 1
+test1 dd 4095
 section .bss
-numa resb 8
-numa_len resb 1
 mas resb 10
-count resb 1
 section .text
 global _start
 _start:
 
-mov byte[mas + 9], ' '
 
-mov eax, 4
-mov ebx, 1
-mov ecx, req
-mov edx, req_len
-int 0x80
-
-;variable assigment
-mov eax, 3
-mov ebx, 0
-mov ecx, numa
-mov edx, 8
-int 0x80
-
-;numa lengh assigment
-mov [numa_len], eax
-
-;counter from 8 to 0
-mov ebx, 8
+mov ebx, 16
+mov ecx, 9
 
 _loop:
 mov al, [cnst]
-mov ecx, [count]
-and al, [numa + ecx]
-add al, 48
-mov [mas + ebx], al
+and al, [test1]
+add al, byte 48
+mov [mas + ecx], al
 
 dec ebx
+dec ecx
 cmp ebx, 0
 je _print
 
-shr byte[numa + ecx], 1
+shr word[test1], 1
 jmp _loop
 
 _print:
@@ -52,12 +31,6 @@ mov ebx, 1
 mov ecx, mas
 mov edx, 10
 int 0x80
-
-mov eax, [numa_len]
-inc byte[count]
-mov ebx, 8
-cmp [count], al
-jne _loop
 
 ;_exit:
 mov eax, 1
